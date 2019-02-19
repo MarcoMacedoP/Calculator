c76f180870d55f06c6a7fcf5e7d5106031f5205e
Marco Antonio Macedo Preciado <marcosiegman01@gmail.com>
2019-02-19 03:33:36 -0600

    Plus added correctly

--- app/src/main/java/k/pm6f1/calculadora_c/Calculadora_simple.java
+++ app/src/main/java/k/pm6f1/calculadora_c/Calculadora_simple.java
@@ -2,15 +2,184 @@ package k.pm6f1.calculadora_c;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import k.pm6f1.calculadora_c.Models.data;
 
 public class Calculadora_simple extends AppCompatActivity {
-
-
+    //Vars declaration
+    int value1, value2;
+    public data object= new data();
+    //Views declaration
+    EditText et_big, et_small;
+    Button btn_c,btn_back, btn_plus, btn_equal,
+            btn_0,btn_1, btn_2,btn_3,btn_4,btn_5,btn_6,
+            btn_7,btn_8,btn_9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora_simple);
-        //button declaration
+        //relating views with code, setting onClickListener when it's necesary
+        et_big=findViewById(R.id.et_big);
+        et_small=findViewById(R.id.et_small);
 
+        btn_back= findViewById(R.id.btn_back);
+        //Faltan muchos botones de operaciones por agregar
+        btn_plus= findViewById(R.id.btn_plus);
+        btn_plus.setOnClickListener(suma);
+        btn_equal= findViewById(R.id.btn_equal);
+        btn_equal.setOnClickListener(igual);
+        //numbers buttons
+        btn_0=findViewById(R.id.btn_0);
+        btn_0.setOnClickListener(numero_cero);
+
+        btn_1=findViewById(R.id.btn_1);
+        btn_1.setOnClickListener(numero_uno);
+
+        btn_2=findViewById(R.id.btn_2);
+        btn_2.setOnClickListener(numero_dos);
+
+        btn_3=findViewById(R.id.btn_3);
+        btn_3.setOnClickListener(numero_tres);
+
+        btn_4=findViewById(R.id.btn_4);
+        btn_4.setOnClickListener(numero_cuatro);
+
+        btn_5=findViewById(R.id.btn_5);
+        btn_5.setOnClickListener(numero_cinco);
+
+        btn_6=findViewById(R.id.btn_6);
+        btn_6.setOnClickListener(numero_seis);
+
+        btn_7=findViewById(R.id.btn_7);
+        btn_7.setOnClickListener(numero_siete);
+
+        btn_8=findViewById(R.id.btn_8);
+        btn_8.setOnClickListener(numero_ocho);
+
+        btn_9=findViewById(R.id.btn_9);
+        btn_9.setOnClickListener(numero_nueve);
     }
+    private View.OnClickListener numero_cero = new View.OnClickListener() {
+        public void onClick(View v) {
+            et_big.setText("0");
+        }
+    };
+    private View.OnClickListener numero_uno = new View.OnClickListener() {
+        public void onClick(View v) {
+
+
+            et_big.setText("1");
+
+
+        }
+    };
+    private View.OnClickListener numero_dos = new View.OnClickListener() {
+        public void onClick(View v) {
+            et_big.setText("2");
+        }
+    };
+    private View.OnClickListener numero_tres = new View.OnClickListener() {
+        public void onClick(View v) {
+            et_big.setText("3");
+        }
+    };
+    private View.OnClickListener numero_cuatro = new View.OnClickListener() {
+        public void onClick(View v) {
+            et_big.setText("4");
+        }
+    };
+    private View.OnClickListener numero_cinco = new View.OnClickListener() {
+        public void onClick(View v) {
+            et_big.setText("5");
+        }
+    };
+    private View.OnClickListener numero_seis = new View.OnClickListener() {
+        public void onClick(View v) {
+            et_big.setText("6");
+        }
+    };
+    private View.OnClickListener numero_siete = new View.OnClickListener() {
+        public void onClick(View v) {
+            et_big.setText("7");
+        }
+    };
+    private View.OnClickListener numero_ocho = new View.OnClickListener() {
+        public void onClick(View v) {
+            et_big.setText("8");
+        }
+    };
+    private View.OnClickListener numero_nueve = new View.OnClickListener() {
+        public void onClick(View v) {
+            et_big.setText("9");
+        }
+    };
+    private View.OnClickListener suma = new View.OnClickListener(){
+        public void onClick(View v) {
+                    int val=Integer.valueOf(
+                    et_big.getText().toString());
+                    String introduced_txt=et_big.getText().toString();
+
+
+
+            if (isNumeric(introduced_txt) == true) {
+
+                object.setValue_1(val);
+                et_small.setText(Integer.toString(object.getValue_1()));
+
+                Toast.makeText(getApplicationContext(), "Added value 1: "+object.getValue_1(), Toast.LENGTH_SHORT).show();
+                et_big.setText("");
+
+
+            } else {
+                Toast.makeText(getApplicationContext(), "Viejo lesbiano",Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    };
+    public static boolean isNumeric(String cadena) {
+
+        boolean resultado;
+
+        try {
+            Integer.parseInt(cadena);
+            resultado = true;
+        } catch (NumberFormatException excepcion) {
+            resultado = false;
+        }
+
+        return resultado;
+    }
+
+    public View.OnClickListener igual= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            String introduced_txt=et_big.getText().toString();
+
+            if (isNumeric(introduced_txt) == true) {
+                int val=Integer.valueOf(
+                        et_big.getText().toString());
+
+                object.setValue_2(val);
+                et_small.setText(Integer.toString(object.getValue_1()));
+
+                Toast.makeText(getApplicationContext(), "Added value 2 & 1: "+object.getValue_1() + "," +object.getValue_2(), Toast.LENGTH_SHORT).show();
+                et_big.setText(Integer.toString(object.getValue_2()+object.getValue_1()));
+
+
+
+            } else {
+                Toast.makeText(getApplicationContext(), "Viejo lesbiano",Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    };
 }
+
+
+
+
---
+++ app/src/main/java/k/pm6f1/calculadora_c/Models/data.java
@@ -0,0 +1,22 @@
+package k.pm6f1.calculadora_c.Models;
+
+public class data {
+    public int value_1;
+    public int value_2;
+
+    public int getValue_1() {
+        return value_1;
+    }
+
+    public void setValue_1(int value_1) {
+        this.value_1 = value_1;
+    }
+
+    public int getValue_2() {
+        return value_2;
+    }
+
+    public void setValue_2(int value_2) {
+        this.value_2 = value_2;
+    }
+}
