20605401a77f238eef31e0b5c75223d03e860dfa
Marco Antonio Macedo Preciado <marcosiegman01@gmail.com>
2019-02-19 00:51:32 -0600

    Interface of first activity

---
+++ app/src/main/java/k/pm6f1/calculadora_c/Calculadora_simple.java
@@ -0,0 +1,16 @@
+package k.pm6f1.calculadora_c;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calculadora_simple extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora_simple);
+        //button declaration
+
+    }
+}
---
+++ app/src/test/java/k/pm6f1/calculadora_c/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package k.pm6f1.calculadora_c;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
