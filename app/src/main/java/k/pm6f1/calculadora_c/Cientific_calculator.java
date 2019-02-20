0f5dff5f506b0d3e6d98991b22b214c12f6e9f7f
Marco Antonio Macedo Preciado <marcosiegman01@gmail.com>
2019-02-20 14:27:51 -0600

    cientific opcions, basic

--- app/src/main/java/k/pm6f1/calculadora_c/Cientific_calculator.java
+++ app/src/main/java/k/pm6f1/calculadora_c/Cientific_calculator.java
@@ -13,10 +13,13 @@ public class Cientific_calculator extends AppCompatActivity {
 
     EditText et_big, et_small;
     //Variables utilizadas
-    String operation; public data object= new data();
+    String operation;
+    public data object= new data();
+    public double result;
     //functions buttons
     Button btn_c,btn_back, btn_plus, btn_equal, btn_f, btn_less, btn_x, btn_div;
-
+    //scientic buttons
+    Button btn_yx_2, btn_reciproc, btn_root, btn_enesim, btn_log; //Faltan botones
 
 
     @Override
@@ -76,6 +79,74 @@ public class Cientific_calculator extends AppCompatActivity {
         }
     };
 
+    //Cientific operations
+    public View.OnClickListener yx= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            save_value();
+            operation="yx";
+
+        }
+    };
+    public View.OnClickListener reciproc= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            save_value();
+            result=1/object.getValue_1();
+            et_big.setText(Double.toString(result));
+            operation="reciproc";
+
+        }
+    };
+    public View.OnClickListener root= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            save_value();
+            operation="root";
+
+        }
+    };
+    public View.OnClickListener enesim= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            save_value();
+            operation="enesim";
+
+        }
+    };
+    public View.OnClickListener log= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            save_value();
+            operation="log";
+
+        }
+    };
+    public View.OnClickListener ln= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            save_value();
+            operation="ln";
+
+        }
+    };
+    public View.OnClickListener Ex= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            save_value();
+            operation="ex";
+
+        }
+    };
+    public View.OnClickListener tenx = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            save_value();
+            operation="10x";
+
+        }
+    };
+
 
     public static boolean isNumeric(String cadena) {
 
@@ -119,30 +190,65 @@ public class Cientific_calculator extends AppCompatActivity {
             String introduced_txt=et_big.getText().toString();
 
             if (isNumeric(introduced_txt) == true) {
-                int val=Integer.valueOf(
-                        et_big.getText().toString());
-
-                object.setValue_2(val);
-                et_small.setText(Integer.toString(object.getValue_1()));
-
-                Toast.makeText(getApplicationContext(), "Added value 2 & 1: "+object.getValue_1() + "," +object.getValue_2(), Toast.LENGTH_SHORT).show();
 
                 if(operation.equals("suma")) {
+                    save_value2();
                     et_big.setText(Integer.toString(object.getValue_2() + object.getValue_1()));
                 }
                 if (operation.equals("resta")){
+                    save_value2();
                     et_big.setText(Integer.toString(object.getValue_2() - object.getValue_2()));
                 }
                 if (operation.equals("multi")){
+                    save_value2();
                     et_big.setText(Integer.toString(
                             object.getValue_2()* object.getValue_1()));
                 }
 
                 if (operation.equals("divi")){
+                    save_value2();
                     et_big.setText(Integer.toString(
                             object.getValue_2() / object.getValue_1()));
                 }
+                if(operation.equals("yx")){
+                    save_value2();
+                    result=Math.pow(object.getValue_1(),object.getValue_2());;//insert operation here
+                    et_big.setText(Double.toString(result));
+                }
+                if(operation.equals("reciproc")){
+                    result=1/object.getValue_1();
+                    et_big.setText(Double.toString(result));
+            }
+                if(operation.equals("root")){
+                    result=Math.sqrt(object.getValue_1());//insert operation here
+                    et_big.setText(Double.toString(result));
+                }
 
+                //Math.pow(16,1.0/4)=2
+                if(operation.equals("enesim")){
+                    save_value2();
+                    result =Math.pow(object.getValue_1(), 1.0/object.getValue_2());//insert operation here
+                    et_big.setText(Double.toString(result));
+                }
+                if(operation.equals("log")){
+                    result= (Math.log10(object.getValue_1()) / Math.log10(10));//insert operation here
+                    et_big.setText(Double.toString(result));
+                }
+                if(operation.equals("ln")){
+                    double e = 2.718282;
+                    result= (Math.log10(object.getValue_1()) / Math.log10(e));//insert operation here
+                    et_big.setText(Double.toString(result));
+                }
+                if(operation.equals("ex")){
+                    double e = 2.718282;
+                    result=Math.pow(e, object.getValue_1());//insert operation here
+                    et_big.setText(Double.toString(result));
+                }
+                if(operation.equals("10x")){
+                    result=(Math.pow(10, object.getValue_1()));
+                    //insert operation here
+                    et_big.setText(Double.toString(result));
+                }
 
             } else {
                 Toast.makeText(getApplicationContext(), "Viejo lesbiano",Toast.LENGTH_SHORT).show();
@@ -150,5 +256,29 @@ public class Cientific_calculator extends AppCompatActivity {
             }
         }
     };
+
+    public boolean save_value2(){
+        boolean result;
+        int val=Integer.valueOf(
+                et_big.getText().toString());
+        String introduced_txt=et_big.getText().toString();
+
+        if (isNumeric(introduced_txt) == true) {
+
+            object.setValue_2(val);
+            et_small.setText(Integer.toString(object.getValue_1()));
+
+            Toast.makeText(getApplicationContext(), "Added value 2 & 1: "+object.getValue_1() + "," +object.getValue_2(), Toast.LENGTH_SHORT).show();
+
+            result=true;
+
+        } else {
+            Toast.makeText(getApplicationContext(), "Viejo lesbiano",Toast.LENGTH_SHORT).show();
+            result= false;
+        }
+        return result;
+    }
+
+
 
 }
